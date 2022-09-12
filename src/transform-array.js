d1e8f85a3a7548e68f50af1e8e8d83f3822fb6a4
ferka123 <ferka911@gmail.com>
2022-09-12 15:00:38 +0300

    feat: completed all tasks

--- src/transform-array.js
+++ src/transform-array.js
@@ -13,7 +13,24 @@ const { NotImplementedError } = require("../extensions/index.js");
  * transform([1, 2, 3, '--discard-prev', 4, 5]) => [1, 2, 4, 5]
  *
  */
-function transform(arr) {}
+function transform(arr) {
+  if (!Array.isArray(arr))
+    throw new Error("'arr' parameter must be an instance of the Array!")
+  const res = [];
+  arr.map((element, index) => {
+    if (arr[index - 1] === "--discard-next") {
+      res.push([]);
+    } else if (element === "--discard-prev") {
+      res.pop();
+    } else if (arr[index - 1] === "--double-next") {
+      res.push(element, element);
+    } else if (element === "--double-prev") {
+      res.push(res.at(-1) ?? []);
+    } else if (element !== "--double-next" && element !== "--discard-next")
+      res.push(element);
+  });
+  return res.flat()
+}
 
 module.exports = {
   transform,
--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,35 +1,64 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const { NotImplementedError } = require("../extensions/index.js");
 
 /**
  * Implement class VigenereCipheringMachine that allows us to create
  * direct and reverse ciphering machines according to task description
- * 
+ *
  * @example
- * 
+ *
  * const directMachine = new VigenereCipheringMachine();
- * 
+ *
  * const reverseMachine = new VigenereCipheringMachine(false);
- * 
+ *
  * directMachine.encrypt('attack at dawn!', 'alphonse') => 'AEIHQX SX DLLU!'
- * 
+ *
  * directMachine.decrypt('AEIHQX SX DLLU!', 'alphonse') => 'ATTACK AT DAWN!'
- * 
+ *
  * reverseMachine.encrypt('attack at dawn!', 'alphonse') => '!ULLD XS XQHIEA'
- * 
+ *
  * reverseMachine.decrypt('AEIHQX SX DLLU!', 'alphonse') => '!NWAD TA KCATTA'
- * 
+ *
  */
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  constructor(mode = true) {
+    this.mode = mode;
   }
-  decrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  encrypt(msg, key) {
+    if (!msg || !key) throw new Error("Incorrect arguments!");
+    key = key.toUpperCase();
+    let totalEnc = 0;
+    const encArray = [...msg.toUpperCase()].map(c => {
+      if (/[A-Z]/.test(c)) { 
+        let encChar =
+          c.charCodeAt(0) + key.charCodeAt(totalEnc % key.length) - 65;
+        if (encChar > 90) encChar -= 26;
+        totalEnc++;
+        return String.fromCharCode(encChar);
+      }
+      return c;
+    });
+    if (this.mode) return encArray.join("")
+    else return encArray.reverse().join("");
+  }
+  decrypt(msg, key) {
+    if (!msg || !key) throw new Error("Incorrect arguments!");
+    key = key.toUpperCase();
+    let totalDec = 0;
+    const decArray = [...msg.toUpperCase()].map(c => {
+      if (/[A-Z]/.test(c)) { 
+        let decChar =
+          c.charCodeAt(0) - key.charCodeAt(totalDec % key.length) + 65;
+        if (decChar < 65) decChar += 26;
+        totalDec++;
+        return String.fromCharCode(decChar);
+      }
+      return c;
+    });
+    if (this.mode) return decArray.join("")
+    else return decArray.reverse().join("");
   }
 }
 
 module.exports = {
-  VigenereCipheringMachine
+  VigenereCipheringMachine,
 };
