72f1e69e112603ff5bf9efedf48d58b2cf686687
ferka123 <ferka911@gmail.com>
2022-09-13 18:09:17 +0300

    refactor: cipher

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -23,27 +23,27 @@ class VigenereCipheringMachine {
   constructor(mode = true) {
     this.mode = mode;
   }
-  encDec(op,msg,key) {
+  encDec(op, msg, key) {
     if (!msg || !key) throw new Error("Incorrect arguments!");
     key = key.toUpperCase();
-    let totalEncDec = 0;
-    const encDecArray = [...msg.toUpperCase()].map(c => {
-      if (/[A-Z]/.test(c)) { 
-        let encDecChar =
-          (c.charCodeAt(0) - op*key.charCodeAt(totalEncDec % key.length)+op*130) % 26;
-        totalEncDec++;
-        return String.fromCharCode(encDecChar+65);
+    let keyShift = 0;
+    const encDecArray = [...msg.toUpperCase()].map((c) => {
+      const letterPos = c.charCodeAt(0);
+      if (letterPos >= 65 && letterPos < 91) {
+        const shift = op * (key.charCodeAt(keyShift++ % key.length) - 130);
+        const newLetterPos = (letterPos - shift) % 26;
+        return String.fromCharCode(newLetterPos + 65);
       }
       return c;
     });
-    if (this.mode) return encDecArray.join("")
+    if (this.mode) return encDecArray.join("");
     else return encDecArray.reverse().join("");
   }
-  encrypt(msg,key) {
-    return this.encDec(-1,msg,key)
+  encrypt(msg, key) {
+    return this.encDec(-1, msg, key);
   }
   decrypt(msg, key) {
-    return this.encDec(1,msg,key)
+    return this.encDec(1, msg, key);
   }
 }
 
